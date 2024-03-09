fe9148dc1d7af16f1a3c5b2707dea7901fa8e99a
ArtfulpixOfficial <143751560+ArtfulpixOfficial@users.noreply.github.com>
2024-03-09 18:31:09 +0530

    Update Message.js

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,7 @@
 export function Message() {
   return (
     <section className="message show">
-      <img src="midyAI.png" alt="midyAI logo" />
+      <img src="logo_dark.png" alt="midyAI logo" />
       <h1 className="text-4xl sm:text-[52px] font-bold font-secondary text-gray-800 dark:text-gray-100 uppercase relative">
         MIDYAI SDXL
       </h1>
