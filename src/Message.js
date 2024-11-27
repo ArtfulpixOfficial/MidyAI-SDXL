430ee3a6d3c3201aec6614a2a1bb964280985e17
Kartikey Jaiswal <karan74jaiswal@gmail.com>
2024-11-28 02:56:34 +0530

    made changes according to presentation

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,12 @@
 export function Message() {
   return (
     <section className="message show">
-      <img src="logo_dark.png" alt="midyAI logo" />
+      <img src="logo.png" alt="midyAI logo" />
       <h1 className="text-4xl sm:text-[52px] font-bold font-secondary text-gray-800 dark:text-gray-100 uppercase relative">
         MIDYAI SDXL
       </h1>
       <h2 className="text-xl font-semibold text-gray-600 dark:text-gray-400">
-        Text-to-Image Creation with a Single Prompt
+        Generate stunning images instantly from a single prompt
       </h2>
     </section>
   );
